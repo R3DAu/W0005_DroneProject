2ebc2e377503ad1c24827d50ebc7093c6e3eefed
R3DAu <mitch@axesis.com.au>
2019-05-15 00:10:14 +1000

    Changed some layouts to the console application. Made it workable and ready for testing

--- ConsoleController/src/Console.cs
+++ ConsoleController/src/Console.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleController.src
@@ -12,38 +13,76 @@ namespace ConsoleController.src
 
         public Console()
         {
+            System.Console.Title = "[WARDCC] W005 - AR Drone Controller Console";
             _controller = new Controller();
+        }
+
+        private void MenuItems()
+        {
+            System.Console.Clear();
+
+            System.Console.WriteLine("\n");
+
+            //let's do some simple status checks here.
+            if (_controller.IsDroneConnected)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine("Drone Connection: Active");
+
+                var percentage = _controller.DroneBatteryPercentage;
+
+                switch (percentage)
+                {
+                    case var _ when percentage < 20.00f:
+                        System.Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("Drone Battery: {0}", percentage.ToString());
+                        break;
+                    case var _ when percentage > 20.01f && percentage < 60.00f:
+                        System.Console.ForegroundColor = ConsoleColor.Yellow;
+                        System.Console.WriteLine("Drone Battery: {0}", percentage.ToString());
+                        break;
+                    case var _ when percentage > 60.01f:
+                        System.Console.WriteLine("Drone Battery: {0}", percentage.ToString());
+                        break;
+                }
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Drone Connection: Not found.");
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("Please select from the following menu items:");
+            System.Console.WriteLine("1. Fly Control");
+            System.Console.WriteLine("2. Exit");
         }
 
         public void Menu()
         {
             int _controlKey;
 
-            System.Console.WriteLine("Please select from the following menu items:");
-            System.Console.WriteLine("1. Start Drone");
-            System.Console.WriteLine("2. Land Drone");
-            System.Console.WriteLine("3. Fly Control");
-
+            Thread.Sleep(200);
+            MenuItems();
+           
             while (!int.TryParse(System.Console.ReadLine(), out _controlKey))
-            {
-                System.Console.Clear();
-                System.Console.WriteLine("Please select from the following menu items:");
-                System.Console.WriteLine("1. Start Drone");
-                System.Console.WriteLine("2. Land Drone");
-                System.Console.WriteLine("3. Fly Control");
-            }
+                MenuItems();
+            
 
             switch (_controlKey)
             {
                 case 1:
+                    _controller.Start();
                     break;
                 case 2:
-                    break;
-                case 3:
-                    _controller.ReadCommandKeys();
-                    break;
+                    return;
             }
-                
+
+            //return back to the menu.
+            Menu();
         }
     }
 }

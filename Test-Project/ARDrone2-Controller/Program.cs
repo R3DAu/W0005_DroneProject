9abf514b2f000ba8fe2bd417f3dc11d544d9efc1
Jake Beltrami <101635513@student.swin.edu.au>
2019-05-22 15:52:49 +1000

    Added the Drone UI

--- Test-Project/ARDrone2-Controller/Program.cs
+++ Test-Project/ARDrone2-Controller/Program.cs
@@ -13,7 +13,7 @@ namespace ARDrone2_Controller
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DroneForm());
         }
     }
 }

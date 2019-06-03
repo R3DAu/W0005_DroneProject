1c1e1d9d090fddb8cab9008f80ad24e075c5b839
Jake Beltrami <101635513@student.swin.edu.au>
2019-06-03 19:00:50 +1000

    Added unit tests for all current functions

--- Test-Project/WARD-UnitTesting/apiTest.cs
+++ Test-Project/WARD-UnitTesting/apiTest.cs
@@ -15,9 +15,9 @@ namespace WARD_UnitTesting
         }
 
         [Test]
-        public void StartCheck()
+        public void LandCheck()
         {
-            Assert.IsTrue(_api.Start());
+            Assert.IsTrue(_api.Land());
         }
 
         [Test]
@@ -26,6 +26,76 @@ namespace WARD_UnitTesting
             Assert.IsTrue(_api.TakeOff());
         }
 
+        [Test]
+        public void StartCheck()
+        {
+            Assert.IsTrue(_api.Start());
+        }
 
+        [Test]
+        public void StopCheck()
+        {
+            Assert.IsTrue(_api.Stop());
+        }
+
+        [Test]
+        public void HoverCheck()
+        {
+            Assert.IsTrue(_api.Hover());
+        }
+
+        [Test]
+        public void PitchForwardCheck()
+        {
+            Assert.IsTrue(_api.PitchForward());
+        }
+
+        [Test]
+        public void PitchBackCheck()
+        {
+            Assert.IsTrue(_api.PitchBack());
+        }
+
+        [Test]
+        public void RollLeftCheck()
+        {
+            Assert.IsTrue(_api.RollLeft());
+        }
+
+        [Test]
+        public void RollRightCheck()
+        {
+            Assert.IsTrue(_api.RollRight());
+        }
+
+        [Test]
+        public void AscendCheck()
+        {
+            Assert.IsTrue(_api.Ascend());
+        }
+
+        [Test]
+        public void DescendCheck()
+        {
+            Assert.IsTrue(_api.Descend());
+        }
+
+        [Test]
+        public void EmergencyCheck()
+        {
+            Assert.IsTrue(_api.Emergency());
+        }
+
+        [Test]
+        public void ResetEmergencyCheck()
+        {
+            Assert.IsTrue(_api.ResetEmergency());
+        }
+
+        [Test]
+        public void JumpCheck()
+        {
+            Assert.IsTrue(_api.Jump());
+        }
     }
 }
